adc7b7cb0371538fbe9dea05334dfb637fee817c
Wang Guangzu <wgz199154@hotmail.com>
2023-01-17 14:41:44 +0800

    add get stream

--- src/main/java/sg/eud/nus/iss/App.java
+++ src/main/java/sg/eud/nus/iss/App.java
@@ -1,14 +1,13 @@
 package sg.eud.nus.iss;
 
-import java.io.BufferedReader;
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOError;
 import java.io.IOException;
-import java.nio.Buffer;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * Hello world!
@@ -20,8 +19,9 @@ public final class App {
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
+     * @throws IOException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
        String dirPath="data2";
 
        File newDirectory =new File(dirPath);
@@ -31,5 +31,38 @@ public final class App {
        }else{
         newDirectory.mkdir();
        }
+
+       Cookie cookie =new Cookie();
+       cookie.readCookieFile();
+       cookie.showCookies();
+
+
+       ServerSocket ss= new ServerSocket(7000);
+       Socket s= ss.accept();
+
+       try(InputStream is= s.getInputStream()){
+            BufferedInputStream bis= new BufferedInputStream(is);
+            DataInputStream dis= new DataInputStream(bis);
+            String msgReceived="";
+
+
+
+            while (!msgReceived.equals("colse")){
+                msgReceived=dis.readUTF();
+
+                if(msgReceived.equalsIgnoreCase("get-cookie")){
+                    String cookieValue=cookie.returnCookie();
+                }
+            }
+        }catch(IOException E){
+            s.close();
+            ss.close();
+        }
+
+
     }
+
+
+
+    
 }
